6d08d07039026617c973099b6e9c7f569f4685f5
Utkarsh Bhandari <utkarsh.b1358@gmail.com>
2022-04-28 14:08:44 +0530

    new

---
+++ src/pages/indexpage.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+const IndexPage = () => (
+    <div>
+        <title>Utkarsh</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </div>
+  );
+
+export default IndexPage;
